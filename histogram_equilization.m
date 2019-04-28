baffebf5b134ef91010afd03847af3c1c57541e2
Yash Srivastava <srivastavayash1234@gmail.com>
2019-04-28 11:13:34 +0530

    Optimized Histogram equilization

--- histogram_equilization.m
+++ histogram_equilization.m
@@ -3,7 +3,6 @@ imgName = input(prompt, 's');
 [filepath, name, ext] = fileparts(imgName);
 Img = imread(imgName);
 [row, col, ch] = size(Img);
-disp('Please wait, it will take some time...');
 
 if ch == 1
     Img = HistGrayImg(Img, row, col);
@@ -36,41 +35,39 @@ end
 imshow(Img);
 
 function grayHE = HistGrayImg(img, row, col)
-    map = containers.Map('KeyType','double','ValueType','double');
-    for i = 0:255
-        map(double(i)) = 0;
-    end
+    FreqVect = zeros(256, 1);
+    
     for i = 1:row
         for j = 1:col
-            map(double(img(i, j))) = map(double(img(i, j))) + 1;
+            FreqVect(img(i, j) + 1, 1) = FreqVect(img(i, j)+1, 1) + 1;
         end
     end
 
     cdf = 0;
-    for i = 0:255
-        if map(i)>0
-            cdf = cdf + map(i);
-            map(i) = cdf;
+    for i = 1:256
+        if FreqVect(i, 1)>0
+            cdf = cdf + FreqVect(i, 1);
+            FreqVect(i, 1) = cdf;
         end
     end
 
     min_cdf = row*col;
-    for i = 0:255
-        if map(i)>0 && map(i) < min_cdf
-            min_cdf = map(i);
+    for i = 1:256
+        if FreqVect(i, 1)>0 && FreqVect(i, 1) < min_cdf
+            min_cdf = FreqVect(i, 1);
             break;
         end
     end
 
-    for i = 0:255
-        if map(i)>0
-            map(i) = histogram_eq(map(i), min_cdf, row, col);
+    for i = 1:256
+        if FreqVect(i, 1)>0
+            FreqVect(i, 1) = histogram_eq(FreqVect(i, 1), min_cdf, row, col);
         end
     end
 
     for i = 1:row
         for j = 1:col
-            img(i, j) = map(double(img(i, j)));
+            img(i, j) = FreqVect(img(i, j)+1, 1);
         end
     end
     grayHE = img;
--- histogram_matching.m
+++ histogram_matching.m
@@ -10,8 +10,6 @@ imgName2 = input(prompt, 's');
 Img2 = imread(imgName2);
 [row2, col2, ch2] = size(Img2);
 
-disp('Please wait, it will take some time...');
-
 if ch1 == 1
     if ch1 == ch2
         map1 = HistGrayImg(Img1, row1, col1);
@@ -56,56 +54,62 @@ end
 imshow(Img1);
 
 function Hist_map = HistGrayImg(img, row, col)
-    map = containers.Map('KeyType','double','ValueType','double');
-    for i = 0:255
-        map(double(i)) = 0;
-    end
+    FreqVect = zeros(256, 1);
+    
     for i = 1:row
         for j = 1:col
-            map(double(img(i, j))) = map(double(img(i, j))) + 1;
+            FreqVect(img(i, j) + 1, 1) = FreqVect(img(i, j)+1, 1) + 1;
         end
     end
 
     cdf = 0;
-    for i = 0:255
-        if map(i)>0
-            cdf = cdf + map(i);
-            map(i) = cdf;
+    for i = 1:256
+        if FreqVect(i, 1)>0
+            cdf = cdf + FreqVect(i, 1);
+            FreqVect(i, 1) = cdf;
         end
     end
 
     min_cdf = row*col;
-    for i = 0:255
-        if map(i)>0 && map(i) < min_cdf
-            min_cdf = map(i);
+    for i = 1:256
+        if FreqVect(i, 1)>0 && FreqVect(i, 1) < min_cdf
+            min_cdf = FreqVect(i, 1);
             break;
         end
     end
 
-    for i = 0:255
-        if map(i)>0
-            map(i) = histogram_eq(map(i), min_cdf, row, col);
+    for i = 1:256
+        if FreqVect(i, 1)>0
+            FreqVect(i, 1) = histogram_eq(FreqVect(i, 1), min_cdf, row, col);
         end
     end
-    Hist_map = map;
+
+    for i = 1:row
+        for j = 1:col
+            img(i, j) = FreqVect(img(i, j)+1, 1);
+        end
+    end
+    Hist_map = FreqVect;
 end
 
 function MappedImg = HistMap(imgIn ,map1, map2, row, col)
     img = uint8(zeros(row, col));
-    temp = 0;
-    for i = 0:255
-        if map1(i)>0 && map2(temp)>0
-            temp = i;
-            while map2(temp) < map1(i) && temp <= 255
+    for i = 1:256
+        if map1(i, 1)>0
+            temp = 1;
+            while map2(temp, 1) < map1(i, 1) && temp <= 255
                 temp = temp + 1;
+                while map2(temp, 1) == 0
+                    temp = temp + 1;
+                end
             end
-            map1(i) = temp;
+            map1(i, 1) = temp;
         end
     end
     
     for i = 1:row
         for j = 1:col
-            img(i, j) = map1(double(imgIn(i, j)));
+            img(i, j) = map1(double(imgIn(i, j)+1));
         end
     end
     MappedImg = img;
